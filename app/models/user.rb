42a01ceca821eefc1626bb20e418b2066bada1e7
Kristine Kozlovskaja <kristinekozlovska@Antons-MacBook-Air.local>
2016-05-18 12:17:07 +0300

    Return to users timeoutable and recoverable methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-  #        :rememberable, :trackable, :validatable,
-  #        :timeoutable, :recoverable, stretches: 20
-
   devise :database_authenticatable, :registerable,
        :recoverable, :rememberable, :trackable, :validatable,
+       :timeoutable, :recoverable,
        :encryptable, :stretches => 30
 end
