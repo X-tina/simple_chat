d66993d0148152078202ab9ec0771a33b88aeaf3
Kristine Kozlovskaja <kristinekozlovska@Antons-MacBook-Air.local>
2016-05-18 11:38:59 +0300

    Hide ability for user to timeout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #        :rememberable, :trackable, :validatable,
+  #        :timeoutable, :recoverable, stretches: 20
+
   devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable,
-         :timeoutable, :recoverable, stretches: 20
+       :recoverable, :rememberable, :trackable, :validatable
 end
