565208c441bd0a62d5bde3b00ff712bf0ee6d19d
Kristine Kozlovskaja <kristinekozlovska@Antons-MacBook-Air.local>
2016-05-18 15:19:50 +0300

    Add simple_token_authentication for API

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Registration, Auth
 gem 'devise'
 gem 'devise-encryptable'
+gem 'simple_token_authentication', '~> 1.0'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+ 
+  protect_from_forgery with: :null_session, if: :request_json?
+  skip_before_filter :verify_authenticity_token, if: :request_json?
+
+  acts_as_token_authentication_handler_for User
+  respond_to :html, :json
+
+  def request_json?
+    request.format == 'application/json'
+  end
 end
