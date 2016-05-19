9cdd4c850ff3c1618e6721badb82b1e84212c399
Kristine Kozlovskaja <kristinekozlovska@Antons-MacBook-Air.local>
2016-05-19 15:38:59 +0300

    Remove provider from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ActiveRecord::Base
        :timeoutable, :recoverable, :omniauthable,
        :encryptable, :stretches => 30,
        :omniauth_providers => [:facebook]
-       
+  
+  has_many :identities
 
   #For Facebook now
   def self.find_for_oauth(auth_hash, signed_in_resource = nil)
@@ -23,9 +24,7 @@ class User < ActiveRecord::Base
                  first_name: auth_hash.extra.raw_info.first_name,
                  last_name: auth_hash.extra.raw_info.last_name,
                  email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth_hash.uid}-#{auth_hash.provider}.com",
-                 # url: auth_hash.info.urls.Facebook,
-                 provider: auth_hash.provider,
-                 password: Devise.friendly_token[0.20]
+                 password: Devise.friendly_token[0,20]
                )
         user.save!
       end
