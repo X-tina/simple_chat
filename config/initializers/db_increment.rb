5f9d75e251dffec0956bbac277a7525789cd87a2
Kristine Kozlovskaja <kristinekozlovska@Antons-MacBook-Air.local>
2016-05-27 16:58:50 +0300

    Custom ID column increment

---
+++ config/initializers/db_increment.rb
@@ -0,0 +1,17 @@
+ActiveRecord::Base.class_eval do
+  def self.reset_autoincrement(options={})
+    table_name = "messages"
+    options[:to] ||= 1
+    case self.connection.adapter_name
+      when 'MySQL'
+        self.connection.execute "ALTER TABLE #{self.table_name} AUTO_INCREMENT=#{options[:to]}"
+      when 'PostgreSQL'
+        self.connection.execute "ALTER SEQUENCE #{self.table_name}_id_seq RESTART WITH #{options[:to]};"
+      when 'SQLite'
+        self.connection.execute "UPDATE sqlite_sequence SET seq=#{options[:to]} WHERE name='#{self.table_name}';"
+      else
+    end
+  end
+end
+
+# Message.reset_autoincrement(:to => 1000) in console or somewhwrw else
