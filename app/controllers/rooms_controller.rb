ddb509ff25b871f446a8443d6a8105e21894455f
Kristine Kozlovskaja <kristinekozlovska@Antons-MacBook-Air.local>
2016-06-03 12:14:08 +0300

    Show last 10 messages

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,6 @@
 class RoomsController < ApplicationController
   def show
-    @messages = Message.all
+    @messages = Message.first(10)
     @users = User.try(:all_who_are_in_touch)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ActiveRecord::Base
-	after_create_commit { MessageBroadcastJob.perform_later(self) }
+  after_create_commit { MessageBroadcastJob.perform_later(self) }
+  default_scope { order('created_at DESC') }
 end
