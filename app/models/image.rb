3c2fee2e522df5662486e9d103820af0b1fb013f
Kristine Kozlovskaja <kristinekozlovska@Antons-MacBook-Air.local>
2016-07-26 12:18:19 +0300

    Save images to S3 bucket

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,8 @@ class Image < ApplicationRecord
 
   has_attached_file :source,
                     styles: { original: "300x300>" },
-                    default_url: ""
+                    default_url: "",
+                    storage: :s3,
+                    s3_credentials: S3_CREDENTIALS
   validates_attachment_content_type :source, content_type: /\Aimage\/.*\Z/
 end
--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,9 +1,11 @@
 S3_CREDENTIALS = { access_key_id: ENV["ACCESS_KEY_ID"],
                    secret_access_key: ENV["SECRET_ACCESS_KEY"],
-                   bucket: ENV["BUCKET"] }
+                   bucket: ENV["BUCKET"],
+                   s3_region: ENV['AWS_REGION'],
+                   storage: :s3,
+                   s3_host_name: "s3-us-west-2.amazonaws.com" }
 
 Aws.config.update({
-  region: 'us-west-2',
   credentials: Aws::Credentials.new(ENV['ACCESS_KEY_ID'], ENV['SECRET_ACCESS_KEY'])  
 })
 
